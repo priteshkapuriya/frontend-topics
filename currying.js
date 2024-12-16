62cf625a634d56ca23471b59ab88f3f397b7c679
priteshkapuriya <34333521+priteshkapuriya@users.noreply.github.com>
2024-12-16 13:23:05 +0530

    Update currying.js

--- currying.js
+++ currying.js
@@ -2,7 +2,7 @@ function sum(...args){
     let a = args.reduce((a,b) => a+b, 0)
     return function(...args){
         let b = args.reduce((a,b) => a+b, 0)
-        if(b){
+        if(args.length > 0){
             return sum(a+b)
         }
         return a;
